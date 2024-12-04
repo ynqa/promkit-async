7c2b11fc1ff85875e66ba03e0734f049272ed680
ynqa <un.pensiero.vano@gmail.com>
2024-12-05 05:05:27 +0900

    feat: CancelableLoadingComponent => LoadingComponent

--- src/component.rs
+++ src/component.rs
@@ -17,48 +17,6 @@ pub trait LoadingComponent: Component {
 
     async fn process_event(&mut self, event_group: &EventGroup) -> Pane;
 
-    async fn run_loading(&mut self, event_group: &EventGroup) -> Pane {
-        let (tx, mut rx) = mpsc::channel::<Pane>(1);
-
-        tokio::select! {
-            _ = async {
-                let mut frame_index = 0;
-                let mut interval = tokio::time::interval(Duration::from_millis(100));
-                loop {
-                    interval.tick().await;
-                    let _ = tx.send(Pane::new(
-                        vec![StyledGraphemes::from(Self::LOADING_FRAMES[frame_index])],
-                        0,
-                    )).await;
-                    frame_index = (frame_index + 1) % Self::LOADING_FRAMES.len();
-                }
-            } => unreachable!(),
-            result = self.process_event(event_group) => {
-                drop(tx);
-                result
-            },
-            Some(loading_pane) = rx.recv() => loading_pane,
-        }
-    }
-
-    async fn run(&mut self, mut rx: mpsc::Receiver<Vec<EventGroup>>, tx: mpsc::Sender<Pane>) {
-        while let Some(event_groups) = rx.recv().await {
-            for event_group in event_groups {
-                let pane = self.run_loading(&event_group).await;
-                if tx.send(pane).await.is_err() {
-                    return;
-                }
-            }
-        }
-    }
-}
-
-#[async_trait]
-pub trait CancelableLoadingComponent: Component {
-    const LOADING_FRAMES: [&'static str; 10] = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
-
-    async fn process_event(&mut self, event_group: &EventGroup) -> Pane;
-
     async fn run_loading(
         &mut self,
         event_group: &EventGroup,
