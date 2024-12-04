9f025cc89e5a683d338d62f1fb495bc9efdb9389
ynqa <un.pensiero.vano@gmail.com>
2024-12-05 03:12:13 +0900

    feat: CancelableLoadingComponent for heavy event-handler

--- src/component.rs
+++ src/component.rs
@@ -18,7 +18,7 @@ pub trait LoadingComponent: Component {
     async fn process_event(&mut self, event_group: &EventGroup) -> Pane;
 
     async fn run_loading(&mut self, event_group: &EventGroup) -> Pane {
-        let (tx, mut rx) = mpsc::channel(1);
+        let (tx, mut rx) = mpsc::channel::<Pane>(1);
 
         tokio::select! {
             _ = async {
@@ -47,6 +47,70 @@ pub trait LoadingComponent: Component {
                 let pane = self.run_loading(&event_group).await;
                 if tx.send(pane).await.is_err() {
                     return;
+                }
+            }
+        }
+    }
+}
+
+#[async_trait]
+pub trait CancelableLoadingComponent: Component {
+    const LOADING_FRAMES: [&'static str; 10] = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+
+    async fn process_event(&mut self, event_group: &EventGroup) -> Pane;
+
+    async fn run_loading(
+        &mut self,
+        event_group: &EventGroup,
+        cancel_rx: &mut mpsc::Receiver<()>,
+    ) -> Option<Pane> {
+        let (loading_tx, mut loading_rx): (mpsc::Sender<Pane>, mpsc::Receiver<Pane>) =
+            mpsc::channel(1);
+
+        tokio::select! {
+            _ = async {
+                let mut frame_index = 0;
+                let mut interval = tokio::time::interval(Duration::from_millis(100));
+                loop {
+                    interval.tick().await;
+                    let _ = loading_tx.send(Pane::new(
+                        vec![StyledGraphemes::from(Self::LOADING_FRAMES[frame_index])],
+                        0,
+                    )).await;
+                    frame_index = (frame_index + 1) % Self::LOADING_FRAMES.len();
+                }
+            } => unreachable!(),
+            result = self.process_event(event_group) => {
+                drop(loading_tx);
+                Some(result)
+            },
+            Some(loading_pane) = loading_rx.recv() => Some(loading_pane),
+            _ = cancel_rx.recv() => {
+                drop(loading_tx);
+                None
+            }
+        }
+    }
+
+    async fn run(&mut self, mut rx: mpsc::Receiver<Vec<EventGroup>>, tx: mpsc::Sender<Pane>) {
+        let mut current_cancel_tx: Option<mpsc::Sender<()>> = None;
+
+        while let Some(event_groups) = rx.recv().await {
+            for event_group in event_groups {
+                if let Some(cancel_tx) = current_cancel_tx.take() {
+                    let _ = cancel_tx.send(()).await;
+                }
+
+                let (new_cancel_tx, mut cancel_rx) = mpsc::channel::<()>(1);
+                current_cancel_tx = Some(new_cancel_tx);
+
+                loop {
+                    if let Some(pane) = self.run_loading(&event_group, &mut cancel_rx).await {
+                        if tx.send(pane).await.is_err() {
+                            return;
+                        }
+                        break;
+                    }
                 }
             }
         }
